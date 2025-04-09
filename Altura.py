ad63a018f030afac414217ccbddac956300cacd5
Josufaria <josufaria18@gmail.com>
2025-04-09 16:08:42 -0300

    Create Altura.py

---
+++ Altura.py
@@ -0,0 +1,6 @@
+print("Digite sua altura aqui:")
+altura = float(input())
+if altura > 1.65:
+    print("Você pode andar nesse brinquedo, se divirta!")
+else:
+    print("Esse brinquedo não é pra crianças, sai daqui toquinho")
